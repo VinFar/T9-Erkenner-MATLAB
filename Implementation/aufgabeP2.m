fcb8fddd255b21c6f70b60582488b7b14322ee39
Robin Weiss <github@robinweiss.de>
2017-05-02 16:56:16 +0200

    Move partitioning to new script

--- Implementation/aufgabeP2.m
+++ Implementation/aufgabeP2.m
@@ -85,26 +85,10 @@ words = lower(words);
 % Compute number of words
 nWords = length(words);
 
-% Partition the training and test set. Use hold-out p = 30%
-partition = cvpartition(nWords, 'HoldOut', 0.3);
 
-% Alternatively:
-% Divide the observations into k disjoint subsamples (or folds), chosen
-% randomly but with roughly equal size.
-% partition = cvpartition(nWords, 'KFold', 10); 
 
-% Choose first fold in case of k-fold partitioning and extract indices for
-% training and test samples
-idxTrain = training(partition, 1);
-idxTest = test(partition, 1);
 
-% Compose train and test sets
-trainSet = words(idxTrain);
-testSet = words(idxTest);
 
-% Compute number of samples
-nTrainSet = length(trainSet);
-nTestSet = length(testSet);
 
 % Define array that stores words comprised of input symbols that map to the
 % respective character.
