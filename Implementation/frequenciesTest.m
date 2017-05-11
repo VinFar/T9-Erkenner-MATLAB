0364a8b6090096ca7abd536955046671631ed3ce
Rob Weiss <github@robinweiss.de>
2017-05-11 23:05:29 +0200

    Implement truncated probability of a sequence recursively

--- Implementation/frequenciesTest.m
+++ Implementation/frequenciesTest.m
@@ -14,14 +14,16 @@ load('englishProbabilityTree.mat')
 fprintf('\n\n')
 disp(horzcat(charTree.tostring, freqTree.tostring))
 
-sequence = 'hallo';
-%sequence = 'in_steinfurt_regnet_es_fast_nie'
+%sequence = 'hallo';
+sequence = 'in_steinfurt_regnet_es_fast_nie'
 
 [frequency prevFrequency] = getFrequency(probTree, sequence, 1)
 
 relFrequency = pSequenceTree(probTree, sequence)
 
 relFrequencyRec = exp(-pSequenceTreeRec( probTree, sequence, 1))
+
+relFrequencyRecTrunc = exp(-pSequenceTreeRecTrunc( probTree, sequence, 1, 2))
 
 pCond = pConditionalTree('o', 'hall', probTree)
 
