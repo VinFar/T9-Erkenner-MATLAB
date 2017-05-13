4b81f5912cbd0b0f060f3ef4b38bcc258f8689d4
Rob Weiss <github@robinweiss.de>
2017-05-13 15:03:12 +0200

    Fix probability computation using n-grams

--- Implementation/pSequenceTreeRecTrunc.m
+++ Implementation/pSequenceTreeRecTrunc.m
@@ -1,38 +1,26 @@
-function p = pSequenceTreeRecTrunc( probTree, sequence, currentIndex, truncAfter)
+function p = pSequenceTreeRecTrunc( probTree, sequence, currentIndex, nGram)
 % pSequence computes the negative natural logarithm of the probability
 % of a sequence of characters given a text corpus
 
 n = length(sequence);
 
-% If length of sequence = 1 only one character exists and there is no
-% condition
-if n == 1
+% Only one character or unigram
+if n == 1 || nGram == 1
     
-    newChar = sequence(1);
+    newChar = sequence(end);
     condition = '';
     
-    % Else if the sequence has at least two elements and the number of
-    % elements in the condition + 1 is larger than the sequence
-elseif n - truncAfter - 1 < 1
-    
-    newChar = sequence(end);
-    condition = sequence(1:end-1);
-    
 else
-    
     newChar = sequence(end);
-    condition = sequence(end-truncAfter-1:end-1);
-    
+    condition = sequence(max(1, end-nGram+1):end-1);
 end
 
 p = - log(pConditionalTree(newChar, condition, probTree));
 
 if n == 1
-    
     return
-    
 end
 
-p = p + pSequenceTreeRec(probTree, sequence(1:end-1), currentIndex);
+p = p + pSequenceTreeRecTrunc(probTree, sequence(1:end-1), currentIndex, nGram);
 
 end
